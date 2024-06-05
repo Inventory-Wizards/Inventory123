117124c70b27ad7f265172a2e0eb2d005cfb22df
DAskey91 <daniel.askey@barclays.com>
2024-06-05 09:57:21 +0100

    Delete function built and working

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,8 +7,32 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [items, setItems] = useState([]);
+	const [currentItem, setCurrentItems] = useState(null);
 
-	
+	async function deleteItem(id){
+		const response = await fetch(`${apiURL}/items/${id}` , {
+			method: "DELETE",
+		})
+
+		const filteredItems = items.filter(item => {
+			if(item.id === id){
+				return false;
+			} else {
+				return true
+			}
+		})
+
+		setItems(filteredItems)
+		setCurrentItems(null)
+	}
+
+	function confirmDelete(id){
+		const confirmed = window.confirm("Are you sure you want to delete this item?");
+
+		if (confirmed){
+			deleteItem(id)
+		}
+	}
 
 	useEffect(() => {
 		async function fetchItems(){
@@ -25,19 +49,39 @@ export const App = () => {
 		fetchItems();
 	}, []);
 
+	if (currentItem){
+		return(
+			<main>
+				<h1>{currentItem.name}</h1>
+				<p>£{currentItem.price.toFixed(2)}</p>
+				<p>{currentItem.description}</p>
+				<img src={currentItem.image} alt=""/>
+				<p>
+				<button onClick={() => setCurrentItems(null)}>All Items</button>
+				</p>
+				<p>
+				<button onClick={() => confirmDelete(currentItem.id)}>Delete Item</button>	
+				</p>
+			</main>
+		)
+	}
+
+
 	return (
 		<main>	
       <h1>Inventory App</h1>
 		<ul>
 	 			 {items.map(item => (
 				<li key={item.id}>
-					<h2>{item.name}</h2>
+					<h2>
+					<button onClick={()=> setCurrentItems(item)}>{item.name}</button>
+					</h2>
 					<img src={item.image} alt="" />
 				</li>
 	  			))}
 	    </ul>
 			
-  <h1>Add an Item</h1>
+  <h3>Add an Item</h3>
   <form id="addItemForm">
   <label for="image">Image:</label>
   <input type="file" id="image" name="image" accept="image/*" required />
